d8dc525af94c2450364f000ff1020041fb2a5edc
libukai <xiaobuyao@gmail.com>
2016-09-23 14:28:20 +0800

    第一个版本，灵活性有限，但可以用于下载我们自己的视频

---
+++ XiaomiZhibo/clip.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# @Author: xiaobuyao
+# @Date:   2016-09-20 15:55:16
+# @Last Modified by:   xiaobuyao
+# @Last Modified time: 2016-09-21 11:39:13
+
+import requests
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool
+import os
+import shutil
+
+def cliplist(roomid):
+    originallist = 'http://playback.ks.zb.mi.com/record/live/' + str(roomid) + '/hls/' + str(roomid) + '.m3u8'
+    listfile = requests.get(originallist)
+    finallist = []
+    for link in listfile.text.split('\n'):
+        if link[0:3] == roomid[8:11]:
+            finallist.append('http://playback.ks.zb.mi.com/record/live/' + str(roomid) + '/hls/' + link)
+    return finallist
+
+def download(filelink):
+    clipcontent = requests.get(filelink)
+    filename = os.path.join(roomid, filelink.split('/')[-1])
+    with open(filename, 'ab') as clip:
+        clip.write(clipcontent.content)
+        # print('已获取文件 ' + filename)
+
+def connect(roomid):
+    print('正在合并视频文件，请耐心等待……')
+    cmd = r'cat %s/*.ts > %s.ts' %(roomid, roomid)
+    os.system( cmd )
+
+if __name__ == '__main__':
+    roomid = input('请输入房间号：')
+    if os.path.isdir(roomid):
+        pass
+    else:
+        os.mkdir(roomid)
+    print('正在下载视频片段，请耐心等待……')
+    pool = Pool(8)
+    pool.map(download, cliplist(roomid))
+    connect(roomid)
+    print('正在删除临时文件，请耐心等待……')
+    shutil.rmtree('%s' %(roomid))
